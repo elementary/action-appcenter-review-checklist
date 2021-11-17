1a0d49f8907ca897ccb0f31ab6314651dd0728a5
Josip Antoliš <josip.antolis@protonmail.com>
2021-11-17 05:43:19 +0100

    fix: empty pull request comment (#1)
    Check if pull request body is null or undefined before checking if it contains the body break. Later on, when appending the checklist, skip original body if it was null or undefined.

--- test/index.js
+++ test/index.js
@@ -3,9 +3,12 @@ const nock = require('nock')
 const { resolve } = require('path')
 
 const PULL_REQUEST_MOCK = require('./pull-request.json')
+const EXPECTED_UPDATE = {
+    body: 'Please pull these awesome changes in!\n\n<!-- appcenter-review-checklist -->\n\ntesting!'
+}
 const PULL_REQUEST_CHECKLIST_MOCK = {
   ...PULL_REQUEST_MOCK,
-  body: 'Please pull these awesome changes in!\n\n<!-- appcenter-review-checklist -->\n\ntesting!'
+  ...EXPECTED_UPDATE
 }
 
 let run = null
@@ -27,7 +30,7 @@ test('adds checklist to PR body without it', async (t) => {
   nock('https://api.github.com')
     .get('/repos/github/testing/pulls/2')
     .reply(200, () => PULL_REQUEST_MOCK)
-    .patch('/repos/github/testing/pulls/2')
+    .patch('/repos/github/testing/pulls/2', EXPECTED_UPDATE)
     .reply(200, () => PULL_REQUEST_CHECKLIST_MOCK)
 
   const result = await run()
@@ -41,4 +44,24 @@ test('does not add checklist to PR body with it already included', async (t) => 
 
   const result = await run()
   t.false(result)
+})
+
+test('adds checklist to PR with null body', async (t) => {
+  const pullRequestWithoutBodyMock = require('./pull-request-without-body.json')
+  const expectedUpdate = {
+      body: '<!-- appcenter-review-checklist -->\n\ntesting!'
+  }
+  const pullRequestWithChecklistMock = {
+      ...pullRequestWithoutBodyMock,
+      ...expectedUpdate
+  }
+
+  nock('https://api.github.com')
+    .get('/repos/github/testing/pulls/2')
+    .reply(200, () => pullRequestWithoutBodyMock)
+    .patch('/repos/github/testing/pulls/2', expectedUpdate)
+    .reply(200, () => pullRequestWithChecklistMock)
+
+  const result = await run()
+  t.true(result)
 })
