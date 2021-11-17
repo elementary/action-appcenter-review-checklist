bd225b69dc87f63191840829d67e4209db8731cb
@elementaryBot <contact@elementary.io>
2021-11-17 04:43:45 +0000

    chore(release): 1.0.1 [skip ci]
    ## [1.0.1](https://github.com/elementary/action-appcenter-review-checklist/compare/v1.0.0...v1.0.1) (2021-11-17)
    
    ### Bug Fixes
    
    * empty pull request comment ([#1](https://github.com/elementary/action-appcenter-review-checklist/issues/1)) ([1a0d49f](https://github.com/elementary/action-appcenter-review-checklist/commit/1a0d49f8907ca897ccb0f31ab6314651dd0728a5))

--- dist/index.js
+++ dist/index.js
@@ -5948,15 +5948,11 @@ const { context, getOctokit } = __nccwpck_require__(438)
 const BODY_BREAK = '<!-- appcenter-review-checklist -->'
 
 function includesChecklist (body) {
-  return body.includes(BODY_BREAK)
+  return body && body.includes(BODY_BREAK)
 }
 
 function addChecklist (body, checklist) {
-  return `${body}
-
-${BODY_BREAK}
-
-${checklist}`
+  return [body, BODY_BREAK, checklist].filter(s => s).join('\n\n')
 }
 
 async function run () {
